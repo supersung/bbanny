4de7db841f486285e24e6cdd38de430d35d6dd51
unknown <bjs73370@gmail.com>
2023-09-25 17:53:51 +0900

    2023/09/25

--- mod/utils.py
+++ mod/utils.py
@@ -1,4 +1,8 @@
 import math
+import random
+from datetime import datetime as dt
+import os
+
 
 def mt_sqrt(x) :
     return math.sqrt(x)
@@ -15,7 +19,6 @@ def mt_exp(x):
 def mt_pi() :
     return math.pi   
 
-import random
 
 def rd_int(x, y):
     return random.radint(x,y)
@@ -29,4 +32,36 @@ def rd_rd():
 def rd_nmvar():
     return random.normalvariate(0,1)
     
+def rd_int(x,y) :
+    return random.randint(x,y)
+
+def rd_list(this) :
+    return random.choice(this)
+
+def rd_rd() :
+    return random.random()
+
+def rd_nmvar() :
+    return random.normalvarlate(0,1)
+
+
+def get_dtnow() :
+    return dt.now()
+
+def cvt_time2str(objtime) :
+    return dt.strptime(objtime, "%Y-%M-%d")
+
+def cvt_str2time() : 
+    obj = dt.now()
+    return obj.strftime("%Y-$m-%d")
+
+
+def get_curdir():
+    return os.getcwd()
+
+def os_mkdir(pname) :
+    return os.mkdir(pname)
+
+def os_rmdir(pname) : 
+    return os.rmdir(pname)
 
