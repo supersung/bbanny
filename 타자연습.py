e0f0b905b8f0133d908fcb1057cab346e46fd218
supersung <143788055+supersung@users.noreply.github.com>
2023-12-05 22:48:32 +0900

    타자연습.py

--- 타자연습.py
+++ 타자연습.py
@@ -4,10 +4,10 @@ def play_game(path):
     
     with open(path, "r", encoding="utf-8") as f:
         start_time = time.time()
-        string_count = 0                                                     # 문자열의 개수를 세는 변수
+        string_count = 0                                                     
 
         for line in f.readlines():
-            line = line.strip()                                                # 필요없는 부분 제거
+            line = line.strip()                                                
             if line == "":
                 continue
             string_count = string_count + len(line) 
@@ -24,11 +24,7 @@ def play_game(path):
         elapsed_time = end_time - start_time
     print(int(string_count / elapsed_time * 60))
 
-# 사용하고자 하는 파일 경로를 인자로 넣어 함수 호출
-''' play_game("data/test.txt") '''
- # 실수로 변환
-    
-''' play_game("data/서시.txt") '''
+
     
     
         
